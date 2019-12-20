5ac851ccb7e3736d9cb9b1e676f69916efafb140
hanati <hanati@tezign.com>
2019-12-20 15:11:59 +0800

    add network invoker

---
+++ networks/model_invoke.py
@@ -0,0 +1,28 @@
+"""
+Name : model_invoke.py
+Author  : Hanat
+Contect : hanati@tezign.com
+Time    : 2019-12-20 14:20
+Desc:
+"""
+import cv2
+import numpy as np
+import torch.nn as nn
+from torchvision import models
+
+
+class NetWorkInvoker(object):
+
+    def __init__(self, model_name='resnet50', embding=512, pretrained=True):
+
+        self.base_model = getattr(models, model_name)
+        self.base_model = nn.Sequential(*list(self.base_model(pretrained=pretrained))[:-1])
+        self._linear = nn.Linear(2048, embding)
+
+    def forward(self, x):
+        x = self.base_model(x)
+        y_pred = self._linear(x)
+
+        return y_pred
+
+
