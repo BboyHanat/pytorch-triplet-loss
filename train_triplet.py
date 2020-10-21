64d98dbd5da28443571723c3793e9c83989610d5
hanati <hanati@tezign.com>
2020-10-21 17:44:22 +0800

    add pretrained model

--- train_triplet.py
+++ train_triplet.py
@@ -35,7 +35,7 @@ def train():
     net = NetWorkInvoker(model_name=model_name, embedding=embedding)
     if pretrained_model:
         net.load_weight(pretrained_model, devices=device)
-    loss_triplet = TripletLoss(margin=5)
+    loss_triplet = torch.nn.TripletMarginLoss(margin=1.0, p=2)
 
     if use_gpu:
         device = torch.device("cuda:0")
